0647ceb0cbfbc286f77595875ba186e75cc97f95
unknown <anggisetiyawan02733@gmail.com>
2025-07-02 18:28:01 +0700

    Fix: use dynamic PORT for Railway

--- controller/profileController.js
+++ controller/profileController.js
@@ -3,8 +3,8 @@ const getProfile = (req, res) => {
         name: "Anggi Setiyawan",
         bio: "Mahasiswa Teknik Informatika yang fokus di web dan mobile development.",
         images: [
-                {id : 1, url: "http://localhost:5000/images/my_avatar.jpg"},
-                {id : 2, url: "http://localhost:5000/images/profile.jpg"},
+                {id : 1, url: "server.railway.internal/images/my_avatar.jpg"},
+                {id : 2, url: "server.railway.internal/images/profile.jpg"},
                 ],
         skills: ["JavaScript", "Node.js", "React", "Laravel"],
         email: "anggisetiyawan02733@email.com",
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', (req,res) =>{
 app.use('/api/profile', profileRoutes);
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
